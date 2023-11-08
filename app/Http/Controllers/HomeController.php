df5efbb61cbea419c4723ac91dd513e198f77cb8
Zeeshan khan <zk27159@gmail.com>
2023-11-08 20:21:09 +0500

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,10 +15,8 @@ class HomeController extends Controller
      */
     public function GetUsers()
     {
-        // Get the currently authenticated user
         $currentUser = Auth::user();
 
-        // Query the database to get 10 users (excluding the current user)
         $users = User::where('id', '!=', $currentUser->id)->take(10)->get();
 
         return response()->json($users);
